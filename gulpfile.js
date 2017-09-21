ccdaf53ae8cca9b2af99d9099f19bed1e56fee72
kvin1087 <kaustubh87@gmail.com>
2017-09-20 22:09:55 -0500

    imported jscs and jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
 gulp.task('hello-world', function() {
     console.log('Our first Hello World Task');
