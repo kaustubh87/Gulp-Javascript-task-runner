514084fad7a639494fed4f48f28d8f3a97de477a
kvin1087 <kaustubh87@gmail.com>
2017-09-20 22:17:32 -0500

    Added new task to use jshint and jscs

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,13 @@ var jscs = require('gulp-jscs');
 gulp.task('hello-world', function() {
     console.log('Our first Hello World Task');
 });
+
+gulp.task('vet', function() {
+    gulp.src([
+            './src/**/*.js',
+            './*.js'
+        ])
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+});
