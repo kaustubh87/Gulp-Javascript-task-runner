7b0e5ae4eeb34c42593746705e0c7fb1a7c10adb
kvin1087 <kaustubh87@gmail.com>
2017-09-22 15:02:23 -0500

    Config file to store arrays for js files

---
+++ gulp.config.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+    var config = {
+        //all js
+        alljs: [
+            './src/**/*.js',
+            './*.js'
+        ]
+    };
+
+    return config;
+};
