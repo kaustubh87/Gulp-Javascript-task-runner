8c380e275a5adcbda04b41ec4a7f1fe0f796ae39
kvin1087 <kaustubh87@gmail.com>
2017-09-21 21:10:21 -0500

    Added gulp utility

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
-
+var util = require('gulp-util');
 gulp.task('hello-world', function() {
     console.log('Our first Hello World Task');
 });
 
 gulp.task('vet', function() {
+    log('Analyzing source');
     gulp.src([
             './src/**/*.js',
             './*.js'
@@ -15,3 +16,9 @@ gulp.task('vet', function() {
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
 });
+
+///////////
+
+function log(msg) {
+
+};
