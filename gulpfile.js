b80c519b04feb64c2e54efef6b9533ec35039059
kvin1087 <kaustubh87@gmail.com>
2017-09-22 15:02:42 -0500

    Included the config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
+var config = require('./gulp.config')();
 
 var $ = require('gulp-load-plugins')({ lazy: true });
 
@@ -9,10 +10,7 @@ gulp.task('hello-world', function() {
 
 gulp.task('vet', function() {
     log('Analyzing source');
-    gulp.src([
-            './src/**/*.js',
-            './*.js'
-        ])
+    gulp.src(config.alljs)
         .pipe($.if(args.verbose, $.print()))
         .pipe($.jscs())
         .pipe($.jshint())
